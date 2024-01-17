9523d24902b23ca842c0173ada0fedd3a2bd9068
KIMOUGINOT <mouginotkilian@gmail.com>
2024-01-17 21:40:16 +0100

    changed readme

--- README.md
+++ README.md
@@ -1,7 +1,13 @@
+PGM image rescaler
+------------------
+
 It's a pgm image rescaler studied in the course 'Object Oriented Conception' in Telecom Physique Strasbourg.
 
 How to use it ?
 =============
 
-Drop your images in the image directory and run the main.cpp file after putting the name of your images in the path variables.
+1. Drop your images in the image directory.
+2. Go in src/main.cpp and write the name of your image in the variable imageRef and image.
+3. In your terminal change directory to build/ and execute : 'cmake ..' and then 'make'
+4. Now execute : './../bin/monExe' 
 
--- src/main.cpp
+++ src/main.cpp
@@ -2,14 +2,14 @@
 
 int main(){
 
-    string refImage = "reference.pgm"; //Put your image name here
-    string Image = "floating.pgm"; //Put your image name here
+    string imageRef = "reference.pgm"; //Put your image name here
+    string image = "floating.pgm"; //Put your image name here
     Interpolation* interpo = new InterpolationBilineaire(); //Choice of the interpolation method between Bilineaire and Plus Proche Voisin (PPV)
     Transformation* transfo;
     Similarite* simi = new SimilariteQuadratique(); 
     VecDoub parameter; 
 
-    MyApp recalageImage(refImage, Image, interpo, transfo, simi);
+    MyApp recalageImage(imageRef, image, interpo, transfo, simi);
     parameter = recalageImage.process();
 
     cout << "-- Tx = " << parameter[0] << " -- "  ; //image x translation
