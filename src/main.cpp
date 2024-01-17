40c759e9aac214d87f2cd7db71b55456c9033d50
KIMOUGINOT <mouginotkilian@gmail.com>
2024-01-17 21:28:20 +0100

    write some comments in main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -2,17 +2,19 @@
 
 int main(){
 
-    Interpolation* interpo = new InterpolationBilineaire(); //Choix de l'interpolation entre Bilineaire et Plus Proche Voisin (PPV)
+    string refImage = "reference.pgm"; //Put your image name here
+    string Image = "floating.pgm"; //Put your image name here
+    Interpolation* interpo = new InterpolationBilineaire(); //Choice of the interpolation method between Bilineaire and Plus Proche Voisin (PPV)
     Transformation* transfo;
-    Similarite* simi = new SimilariteQuadratique(); //Choix de la similarite : pour l'instant, que Quadratique  
-    VecDoub parameter; //tableau qui à terme contiendra [Tx,Ty,theta]
+    Similarite* simi = new SimilariteQuadratique(); 
+    VecDoub parameter; 
 
-    MyApp recalageImage("reference.pgm", "floating.pgm", interpo, transfo, simi); //Mettre obligatoirement les images dans le dossier image
+    MyApp recalageImage(refImage, Image, interpo, transfo, simi);
     parameter = recalageImage.process();
 
-    cout << "-- Tx = " << parameter[0] << " -- "  ;
-    cout << "Ty = " << parameter[1] << " -- " ;
-    cout << "theta = " << parameter[2] << " ° --" << endl ;
+    cout << "-- Tx = " << parameter[0] << " -- "  ; //image x translation
+    cout << "Ty = " << parameter[1] << " -- " ; //image y translation
+    cout << "theta = " << parameter[2] << " ° --" << endl ; //image rotation
 
     return 0;
 }
